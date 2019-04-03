5f7b1dee711b275557ec363e30444034200f0904
ashikur23 <ashiqur.ravil61@gmail.com>
2019-04-03 07:23:19 +0000

    //adding calculation

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-
-int main()
-{
-
-return 0;
-}
+ #include "stdio.h"
+ 
+ int main()
+ {
+     int a=5,b=5,c;
+     
+     c=a+b;
+     printf("The value of c is %d\n",c);
+     
+     return(0);
+ }
